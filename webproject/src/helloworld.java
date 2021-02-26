18f3983c77763b75fcd4daed57faabf1c0f5daef
YusukeYoshioka <46658255+YusukeYoshioka@users.noreply.github.com>
2021-02-26 16:12:48 -0600

    First Test.
    Just write output program.

---
+++ webproject/src/helloworld.java
@@ -0,0 +1,10 @@
+
+public class helloworld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+		System.out.println("I make this program, commit, and push from Eclipse.");
+	}
+
+}
